e1977befba7e220c60ad676dd2e0bbbad66873e7
Huy nq <huy.nguyenquang@evotek.vn>
2023-09-07 23:26:37 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+// get share customer lib for resolver
+export const getShareCustomerLib = () => {
+  return {
+    singleton: true,
+    requiredVersion: '^0.0.1',
+    import: 'projects/hp-share/src/public-api'
+  }
+}
+
 const webpackConfig = {
 
 }
